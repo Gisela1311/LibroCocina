3e480ab5560ffb12604d2d5e803004378d81a4ff
Gisela Palomar <gslpalomar02@gmail.com>
2024-10-08 11:13:38 +0200

    cambios
    en def __init__ hemos implementado que las fechas se actualizen con 3el sistema.
    En def __str__ faltaba aplicar el format para que se printeara correctamente.
    hemos creado el método buscar_receta

--- libro_cocina.py
+++ libro_cocina.py
@@ -1,28 +1,57 @@
 from receta import Receta
+from datetime import datetime
 
 class Libro:
-    def __init__(self, n:str, fc: str, fa:str):
+    def __init__(self, n:str):
+        '''
+        Los elementos que tendrá el libro
+        '''
         self.nombre = n
-        self.fecha_creacion = fc
-        self.fecha_actualización = fa
-        self.recetas = {"entrante":[], "postres":[], "principal": []} #arreglar para hacer el diccionario
+        self.fecha_creacion = datetime.now().strftime("%d/%m/%y  %H:%M:%S.%f")
+        self.fecha_actualización =  self.fecha_creacion
+        self.recetas = {"entrante":[], "postres":[], "principal": []} 
 
     def __str__(self):
-        return self.nombre, self.fecha_creacion, self.fecha_actualización
+        '''
+        Esta será la información qie se mostrará del libro
+        '''
+        return f"{self.nombre}, {self.fecha_creacion}, {self.fecha_actualización}"
     
     def guarda_receta(self, receta:Receta):
+        '''
+        lo usaremos para introducir/guardar una receta en el libro, ordenada por TIPO
+        '''
         if receta.tipo not in self.recetas:
             print(f"El tipo {receta.tipo} NO existe en este libro")
         else:
             self.recetas[receta.tipo].append(receta)
+            self.fecha_actualización = datetime.now().strftime("%d/%m/%y  %H:%M:%S.%f")
 
     
-    def listar_tipo (self):
+    def listar(self):
+        '''
+        Método para listar todas las recetas del libro. No distingue por ningún valor
+        '''
         for listareceta in self.recetas.values():
             for receta in listareceta:
                 print(receta)
+    
+    def listar_tipo(self, tipo):
+        '''
+        Método para listar las recetas del libro por tipo de receta
+        '''
+        for listareceta in self.recetas.values():
+            for receta in listareceta:
+                if receta.tipo ==  tipo:
+                    print (receta)
+        print(f"No existe la cateogria: {tipo}")
+                
+        
 
-    def buscar_receta (self, nombre):
+    def buscar_receta(self, nombre):
+        '''
+        usar para buscar una receta por su nombre
+        '''
         for listareceta in self.recetas.values():
             for receta in listareceta:
                 if receta.nombre ==  nombre:
